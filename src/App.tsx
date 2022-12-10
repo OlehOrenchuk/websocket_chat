86c02c7db337579c20df6604b7f3f4d2cd80aa96
marjanlukavy <marianlukavyi@nerdzlab.com>
2022-12-10 06:51:08 +0200

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!1</h1>
-    </div>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
