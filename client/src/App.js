a8d430e9a47d865e01ff4e7f0945034361132bb6
marjanlukavy <marianlukavyi@nerdzlab.com>
2023-01-27 18:07:05 +0200

    simple websocket chat
    add:
    * websocket server
    * ui part

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Chat from "./components/Chat";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Welcome to React</h1>
+      </header>
+
+      <Chat />
+    </div>
+  );
+}
+
+export default App;
